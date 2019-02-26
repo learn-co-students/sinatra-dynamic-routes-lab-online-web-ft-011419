fc90be6ab2f9392ccdd3b40f2f26451c2d2b0c28
Michael Schiumo <mschiumo18@gmail.com>
2019-02-26 12:34:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -8,8 +8,7 @@ class App < Sinatra::Base
   end
 
   get "/square/:number" do
-    @sqrt = params[:number].to_i
-    erb :square
+    (params[:number].to_i ** 2).to_s
   end
 
   get "/say/:number/:phrase" do
@@ -18,8 +17,7 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @concat = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
-    "#{@concat}"
+     [:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
   end
 
   get "/:operation/:number1/:number2" do
