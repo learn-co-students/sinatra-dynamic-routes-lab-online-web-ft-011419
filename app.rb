bb9bbd655c27327fdb3b0f04a801515db7af13f0
Michael Schiumo <mschiumo18@gmail.com>
2019-02-26 12:38:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -12,12 +12,13 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
-    @phrase = params[:phrase]^params[:number]
-    "#{@phrase}"
+    answer = ''
+    params[:phrase].times.do
+      phrase += 
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-     "#{params[:word1]} #{params[:word2]} #{params[:word3]} + #{params[:word4]} + #{params[:word5]}." 
+     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get "/:operation/:number1/:number2" do
